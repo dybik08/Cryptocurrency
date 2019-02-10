f439d2d31cb93b2047792ddae7597d41a690e109
dybik08 <dybik08@gmail.com>
2019-02-10 14:45:02 +0100

    Refractor picked pair action file

--- src/actions/picked_pair.ts
+++ src/actions/picked_pair.ts
@@ -1,6 +1,6 @@
 export const PICKED_CURRENCY_PAIR = 'PICKED_CURRENCY_PAIR';
 
-export function pickedCurrencyPair(currencyPair){
+export function pickedCurrencyPair(currencyPair: string): object{
     return {
         type: PICKED_CURRENCY_PAIR,
         currencyPair,
